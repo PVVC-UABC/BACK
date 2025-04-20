78c8f4c37739cb5d36d5e0256c143ce244f88a57
oscda1 <oscdapro@gmail.com>
2025-04-20 15:59:15 -0700

    Refactor: adaptacion al nuevo esquema de BD

---
+++ credentials.py
@@ -0,0 +1,20 @@
+__host='proyectosfcqi.tij.uabc.mx'
+__port=3306
+__user='becerra20242'
+__password='2532'
+__db='bd2becerra20242'
+
+def get_host():
+    return __host
+
+def get_port():
+    return __port
+
+def get_user():
+    return __user
+
+def get_password():
+    return __password
+
+def get_db():
+    return __db
